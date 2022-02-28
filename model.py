3e1abf0d053fcd95584002e7cca527c242e4cbb3
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2022-02-28 17:12:32 +0000

    Update model.py

--- model.py
+++ model.py
@@ -90,7 +90,7 @@ class tridim_nn:
         
         x = Dense(200, activation='sigmoid', bias_regularizer=regularizers.l2(1e-7), kernel_regularizer=regularizers.l2(1e-7), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(position)
         x = Dense(200, activation='sigmoid', bias_regularizer=regularizers.l2(1e-7), kernel_regularizer=regularizers.l2(1e-7), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
-        x = Dense(200, activation='sigmoid', bias_regularizer=regularizers.l2(1e-7), kernel_regularizer=regularizers.l2(1e-7), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
+        #x = Dense(200, activation='sigmoid', bias_regularizer=regularizers.l2(1e-7), kernel_regularizer=regularizers.l2(1e-7), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
         x = Dense(200, activation='sigmoid', bias_regularizer=regularizers.l2(1e-7), kernel_regularizer=regularizers.l2(1e-7), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
         x = Dense(200, activation='sigmoid', bias_regularizer=regularizers.l2(1e-7), kernel_regularizer=regularizers.l2(1e-7), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
         x = Dense(3, activation='linear', kernel_regularizer=regularizers.l2(1e-7), kernel_initializer="random_normal", use_bias=False)(x)
