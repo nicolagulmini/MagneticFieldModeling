5381bd2880d1449c0bcb6c7f69d05e9da7ef9f3a
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2022-02-28 17:27:48 +0000

    Update model.py

--- model.py
+++ model.py
@@ -90,14 +90,7 @@ class tridim_nn:
         
         x = Dense(300, activation='sigmoid', bias_regularizer=regularizers.l2(1e-10), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(position)
         x = Dense(300, activation='sigmoid', bias_regularizer=regularizers.l2(1e-10), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
-        x = Dense(300, activation='sigmoid', bias_regularizer=regularizers.l2(1e-10), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
-        
-        x = Dense(300, activation='sigmoid', bias_regularizer=regularizers.l2(1e-10), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
-        x = Dense(300, activation='sigmoid', bias_regularizer=regularizers.l2(1e-10), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
-        x = Dense(300, activation='sigmoid', bias_regularizer=regularizers.l2(1e-10), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
-        x = Dense(300, activation='sigmoid', bias_regularizer=regularizers.l2(1e-10), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
-        
-        
+        x = Dense(300, activation='sigmoid', bias_regularizer=regularizers.l2(1e-10), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)        
         x = Dense(3, activation='linear', kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
         
         model = Model(inputs=position, outputs=x)
