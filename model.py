bb8b80b5bb6ee312b2753bb038eda4445dcf5ac0
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2022-02-28 17:17:59 +0000

    Update model.py

--- model.py
+++ model.py
@@ -88,11 +88,11 @@ class tridim_nn:
         
         position = Input(shape=(3), name='input_position')
         
-        x = Dense(200, activation='sigmoid', bias_regularizer=regularizers.l2(1e-8), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(position)
+        x = Dense(300, activation='sigmoid', bias_regularizer=regularizers.l2(1e-8), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(position)
         #x = Dense(200, activation='sigmoid', bias_regularizer=regularizers.l2(1e-8), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
         #x = Dense(200, activation='sigmoid', bias_regularizer=regularizers.l2(1e-7), kernel_regularizer=regularizers.l2(1e-7), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
-        x = Dense(200, activation='sigmoid', bias_regularizer=regularizers.l2(1e-8), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
-        x = Dense(200, activation='sigmoid', bias_regularizer=regularizers.l2(1e-8), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
+        x = Dense(300, activation='sigmoid', bias_regularizer=regularizers.l2(1e-8), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
+        x = Dense(300, activation='sigmoid', bias_regularizer=regularizers.l2(1e-8), kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
         x = Dense(3, activation='linear', kernel_regularizer=regularizers.l2(1e-8), kernel_initializer="random_normal", use_bias=True, bias_initializer='zeros')(x)
         
         model = Model(inputs=position, outputs=x)
