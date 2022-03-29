a7e3e67304d4101c41c532450e351ac801af46c5
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2022-03-29 17:58:02 +0100

    Add files via upload

--- cube.py
+++ cube.py
@@ -4,8 +4,8 @@ from sklearn.metrics.pairwise import rbf_kernel
 class rbf_interpolator:
     
     def __init__(self, pointset=None, measures=None, gamma=.0005):
-        self.pointset = pointset
-        self.measures = measures
+        self.pointset = None
+        self.measures = None
         if pointset is not None:
             if pointset.shape[0] == measures.shape[0]:
                 self.pointset = pointset
