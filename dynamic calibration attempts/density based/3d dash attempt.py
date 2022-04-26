bb1c247c9a9ec34c10f70f46d178f528053e3651
Nicola Gulmini <nicolagulmini@gmail.com>
2022-04-26 20:09:24 +0100

    almost got it

--- dynamic calibration attempts/density based/3d dash attempt.py
+++ dynamic calibration attempts/density based/3d dash attempt.py
@@ -24,23 +24,31 @@ app.layout = html.Div(html.Div([html.H4('Magnetic Field Freehand Calibration'),
                       )
 fig = make_subplots(rows=1, cols=3, specs=[[{'is_3d': True}, {'is_3d': True}, {'is_3d':True}]])
 
-x = np.random(100)
-y = np.random(100)
-z = np.random(100)
+x = np.random.random(5)
+y = np.random.random(5)
+z = np.random.random(5)
 
-fig.add_trace(go.Scatter3d(x=x, y=y, z=z), 1, 1)
-fig.add_trace(go.Scatter3d(x=x, y=y, z=z), 1, 2)    
-fig.add_trace(go.Scatter3d(x=x, y=y, z=z), 1, 3)
+c = np.random.random(5)
+s = np.random.random(5)*50
+
+color_scale = [[.0, '#FF2D00'], [.5, '#FFF700'], [1.0, '#27FF00']]
+
+fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode='markers', marker=dict(size=s, color=c, colorscale=color_scale, opacity=.5)), 1, 1)
+fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode='markers', marker=dict(size=s, color=c, colorscale=color_scale, opacity=.5)), 1, 2)    
+fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode='markers', marker=dict(size=s, color=c, colorscale=color_scale, opacity=.5)), 1, 3)
 
 
-fig.update_layout(uirevision='dont change')
+fig.update_layout(margin=dict(l=0, r=0, b=0, t=0), uirevision='a')
 
 @app.callback(Output('plot', 'figure'),
               Input('interval-component', 'n_intervals'))
 def update_graph_live(n_intervals):
-    
-    #fig.update_layout(uirevision='dont change')
-    
+    fig['data'][0]['marker']['color'] = np.random.random(5)
+    fig['data'][0]['marker']['size'] = np.random.random(5)*50
+    fig['data'][1]['marker']['color'] = np.random.random(5)
+    fig['data'][1]['marker']['size'] = np.random.random(5)*50
+    fig['data'][2]['marker']['color'] = np.random.random(5)
+    fig['data'][2]['marker']['size'] = np.random.random(5)*50
     return fig
 
 
