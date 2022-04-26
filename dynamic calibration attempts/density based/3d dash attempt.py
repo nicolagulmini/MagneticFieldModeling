82035f3bc66c87325baa59fdba0274ac02dd996e
Nicola Gulmini <nicolagulmini@gmail.com>
2022-04-26 19:38:03 +0100

    update

--- dynamic calibration attempts/density based/3d dash attempt.py
+++ dynamic calibration attempts/density based/3d dash attempt.py
@@ -1,6 +1,8 @@
 import numpy as np
 import plotly.graph_objects as go
 
+import time
+
 import webbrowser
 import dash
 from dash import dcc, html
@@ -8,47 +10,36 @@ from dash.dependencies import Input, Output
 
 from plotly.subplots import make_subplots
 
+
+
 app = dash.Dash()
 
-app.layout = html.Div(
-    html.Div([
-        html.H4('Magnetic Field Freehand Calibration'),
-        # html.Div(id='live-update-text'),
-        dcc.Graph(id='live-update-graph'),
-        dcc.Interval(
-            id='interval-component',
-            interval = 1000, # ms
-            n_intervals = 0
-        )
-    ])
-)
+app.layout = html.Div(html.Div([html.H4('Magnetic Field Freehand Calibration'),
+                                # html.Div(id='live-update-text'),
+                                dcc.Graph(id='plot'),
+                                dcc.Interval(id='interval-component',
+                                             interval = 1000, # ms
+                                             n_intervals = 0)
+                                ])
+                      )
+fig = make_subplots(rows=1, cols=3, specs=[[{'is_3d': True}, {'is_3d': True}, {'is_3d':True}]])
+
+x = np.random(100)
+y = np.random(100)
+z = np.random(100)
+
+fig.add_trace(go.Scatter3d(x=x, y=y, z=z), 1, 1)
+fig.add_trace(go.Scatter3d(x=x, y=y, z=z), 1, 2)    
+fig.add_trace(go.Scatter3d(x=x, y=y, z=z), 1, 3)
 
 
-# testo
-# @app.callback(Output('live-update-text', 'children'),
-#               Input('interval-component', 'n_intervals'))
-# def update_metrics(n):
-#     lon, lat, alt = 0., 0., 0.
-#     style = {'padding': '5px', 'fontSize': '16px'}
-#     return [
-#         html.Span('Longitude: {0:.2f}'.format(lon), style=style),
-#         html.Span('Latitude: {0:.2f}'.format(lat), style=style),
-#         html.Span('Altitude: {0:0.2f}'.format(alt), style=style)
-#     ]
+fig.update_layout(uirevision='dont change')
 
-
-# plots
-@app.callback(Output('live-update-graph', 'figure'),
+@app.callback(Output('plot', 'figure'),
               Input('interval-component', 'n_intervals'))
 def update_graph_live(n_intervals):
     
-    print(n_intervals)
-    
-    fig = make_subplots(rows=1, cols=3, specs=[[{'is_3d': True}, {'is_3d': True}, {'is_3d':True}]])
-    
-    fig.append_trace(go.Scatter3d(x=np.random.random(3), y=np.random.random(3), z=np.random.random(3)), 1, 1)
-    fig.append_trace(go.Scatter3d(x=np.random.random(3), y=np.random.random(3), z=np.random.random(3)), 1, 2)
-    fig.append_trace(go.Scatter3d(x=np.random.random(3), y=np.random.random(3), z=np.random.random(3)), 1, 3)
+    #fig.update_layout(uirevision='dont change')
     
     return fig
 
--- dynamic calibration attempts/density based/main.py
+++ dynamic calibration attempts/density based/main.py
@@ -1,6 +1,5 @@
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from dash.dependencies import Input, Output
 import numpy as np
 
 import webbrowser
@@ -13,53 +12,36 @@ from dash import html
 
 import threading
 
-def f(fig):
+def f(data):
     while True:
-        fig.append_trace(go.Scatter3d(x=np.random.random(3), y=np.random.random(3), z=np.random.random(3)), 1, 1)
-        fig.append_trace(go.Scatter3d(x=np.random.random(3), y=np.random.random(3), z=np.random.random(3)), 1, 2)
-        fig.append_trace(go.Scatter3d(x=np.random.random(3), y=np.random.random(3), z=np.random.random(3)), 1, 3)
-        time.sleep(1)
+        data = np.random.random(100), np.random.random(100), np.random.random(100)
         
 def app_thread(app):
     app.run_server(debug=True, use_reloader=False)
     
 def reload():
-    return html.Div([dcc.Graph(figure=fig)])
+    tmp = html.Div([dcc.Graph(figure=fig)])
+    
         
 if __name__ == '__main__':
     
     fig = make_subplots(rows=1, cols=3, specs=[[{'is_3d': True}, {'is_3d': True}, {'is_3d':True}]])
-
-    fig.update_layout(scene_camera_eye=dict(x=2, y=2, z=2),
-                      scene2_camera_eye=dict(x=2, y=2, z=2),
-                      scene3_camera_eye=dict(x=2, y=2, z=2))
+    data = np.random.random(100), np.random.random(100), np.random.random(100)
     
-    t = threading.Thread(target=f, args=[fig,])
+    t = threading.Thread(target=f, args=(data,))
     t.start()
     
+    fig.add_trace(go.Scatter3d(x=data[0], y=data[1], z=data[2]), 1, 1)
+    fig.add_trace(go.Scatter3d(x=data[0], y=data[1], z=data[2]), 1, 2)    
+    fig.add_trace(go.Scatter3d(x=data[0], y=data[1], z=data[2]), 1, 3)
+    
     app = dash.Dash()
-    app.layout = html.Div(
-        html.Div([
-            html.H4('Magnetic Field Freehand Calibration'),
-            # html.Div(id='live-update-text'),
-            dcc.Graph(id='live-update-graph'),
-            dcc.Interval(
-                id='interval-component',
-                interval=1*1000, # in milliseconds
-                n_intervals=0
-            )
-        ])
-    )
+    app.layout = html.Div([dcc.Graph(figure=fig)])
     
     webbrowser.open('http://127.0.0.1:8050/', new=2)
     
     t2 = threading.Thread(target=app_thread, args=[app,])
     t2.start()
-    
-    @app.callback(Output('live-update-graph', 'figure'),
-                  Input('interval-component', 'n_intervals'))
-    def update_graph_live(n):
-        return fig
     
     while True:
         app.layout = reload
