c7473427876e0deb21fe475c4345d3cca0994a75
Nicola Gulmini <nicola.gulmini@studenti.unipd.it>
2022-04-26 12:28:40 +0100

    Create tutorial animation with plotly and dash on web browser.py

---
+++ dynamic calibration attempts/density based/tutorial animation with plotly and dash on web browser.py
@@ -0,0 +1,35 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
+
+import webbrowser
+
+import dash
+from dash import dcc
+from dash import html
+
+x, y, z = np.mgrid[0:10, 0:10, 0:10]
+x = x.flatten()
+y = y.flatten()
+z = z.flatten()
+
+u = np.zeros_like(x)
+v = np.zeros_like(y)
+w = z**2
+
+fig = make_subplots(rows=1, cols=3, specs=[[{'is_3d': True}, {'is_3d': True}, {'is_3d':True}]])
+
+fig.add_trace(go.Streamtube(x=x, y=y, z=z, u=u, v=v, w=w), 1, 1)
+fig.add_trace(go.Streamtube(x=x, y=y, z=z, u=w, v=v, w=u), 1, 2)
+fig.add_trace(go.Streamtube(x=x, y=y, z=z, u=u, v=w, w=v), 1, 3)
+
+fig.update_layout(scene_camera_eye=dict(x=2, y=2, z=2),
+                  scene2_camera_eye=dict(x=2, y=2, z=2),
+                  scene3_camera_eye=dict(x=2, y=2, z=2))
+
+app = dash.Dash()
+app.layout = html.Div([dcc.Graph(figure=fig)])
+
+webbrowser.open('http://127.0.0.1:8050/', new=2)
+app.run_server(debug=True, use_reloader=False)
+
