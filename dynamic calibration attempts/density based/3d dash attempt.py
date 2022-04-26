04574ec684d88023142673b0fa9440968b968ba1
Nicola Gulmini <nicolagulmini@gmail.com>
2022-04-26 20:56:16 +0100

    Update 3d dash attempt.py

--- dynamic calibration attempts/density based/3d dash attempt.py
+++ dynamic calibration attempts/density based/3d dash attempt.py
@@ -10,31 +10,28 @@ import CoilModel as Coil
 import Queue
 
 AMOUNT_OF_NEW_POINTS = 50
-q = Queue(maxsize = AMOUNT_OF_NEW_POINTS)
-cube = CubeModel.cube_to_calib()
 
+q = Queue(maxsize = AMOUNT_OF_NEW_POINTS)
+global q
+
+cube = CubeModel.cube_to_calib(np.array([-25., 25., 5.]), 50., point_density=20., minimum_number_of_points=5)
+global cube
+
+zline = cube.grid.T[2]
+yline = cube.grid.T[1]
+xline = cube.grid.T[0]
+
+# questo deve essere un processo
 def update():
     while len(q.queue) < AMOUNT_OF_NEW_POINTS:
         pos, ori = cube.origin_corner + cube.side_length*np.random.random(3), np.array([1., 0., 0.]) #np.random.random(3)
         tmp = get_theoretical_field(coil_model, pos, ori)
         q.put(np.concatenate((pos, ori, tmp.A1), axis=0))
 
-new_raw_points = np.array([q.get() for _ in range(AMOUNT_OF_NEW_POINTS)])
-
-cube.add_batch(new_raw_points)
+    new_raw_points = np.array([q.get() for _ in range(AMOUNT_OF_NEW_POINTS)])
+    
+    cube.add_batch(new_raw_points)
   
-c_x = cube.contributions.T[0][np.newaxis] 
-c_y = cube.contributions.T[1][np.newaxis]
-c_z = cube.contributions.T[2][np.newaxis]
-
-unc_x = 1.-np.minimum(c_x, np.ones(c_x.shape))
-unc_y = 1.-np.minimum(c_y, np.ones(c_x.shape))
-unc_z = 1.-np.minimum(c_z, np.ones(c_x.shape))
-
-color_vec_x = np.concatenate((unc_x, 1-unc_x, np.zeros(unc_x.shape)), axis=0).T
-color_vec_y = np.concatenate((unc_y, 1-unc_y, np.zeros(unc_y.shape)), axis=0).T
-color_vec_z = np.concatenate((unc_z, 1-unc_z, np.zeros(unc_z.shape)), axis=0).T
-
 
 
 ####################################################################################
@@ -56,9 +53,13 @@ s = np.random.random(5)*50
 
 color_scale = [[.0, '#FF2D00'], [.5, '#FFF700'], [1.0, '#27FF00']]
 
+# qui metti tutto a zero e' solo per inizializzare
 fig.add_trace(go.Scatter3d(x=xline, y=yline, z=zline, mode='markers', marker=dict(size=(60*i*unc_x)**2, color=color_vec_x, colorscale=color_scale, opacity=.5), name='x'), 1, 1)
 fig.add_trace(go.Scatter3d(x=xline, y=yline, z=zline, mode='markers', marker=dict(size=(60*i*unc_y)**2, color=color_vec_y, colorscale=color_scale, opacity=.5)), 1, 2)    
 fig.add_trace(go.Scatter3d(x=xline, y=yline, z=zline, mode='markers', marker=dict(size=(60*i*unc_z)**2, color=color_vec_z, colorscale=color_scale, opacity=.5)), 1, 3)
+
+
+
 
 # plot sensor
 #fig.add_trace(go.Scatter3d())
@@ -73,9 +74,24 @@ fig.add_trace(go.Scatter3d(x=xline, y=yline, z=zline, mode='markers', marker=dic
 
 fig.update_layout(margin=dict(l=0, r=0, b=0, t=0), showlegend=True, uirevision='_')
 
+
+
 @app.callback(Output('plot', 'figure'),
               Input('interval-component', 'n_intervals'))
 def update_graph_live(n_intervals):
+    
+    # ilcubio e' globale 
+    c_x = cube.contributions.T[0][np.newaxis] 
+    c_y = cube.contributions.T[1][np.newaxis]
+    c_z = cube.contributions.T[2][np.newaxis]
+
+    unc_x = 1.-np.minimum(c_x, np.ones(c_x.shape))
+    unc_y = 1.-np.minimum(c_y, np.ones(c_x.shape))
+    unc_z = 1.-np.minimum(c_z, np.ones(c_x.shape))
+
+    color_vec_x = np.concatenate((unc_x, 1-unc_x, np.zeros(unc_x.shape)), axis=0).T
+    color_vec_y = np.concatenate((unc_y, 1-unc_y, np.zeros(unc_y.shape)), axis=0).T
+    color_vec_z = np.concatenate((unc_z, 1-unc_z, np.zeros(unc_z.shape)), axis=0).T
     
     # update markers' sizes and colors
     fig['data'][0]['marker']['color'] = np.random.random(5)
