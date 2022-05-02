962d5f391459e36c17c8e1c75fd64e544e418483
Nicola Gulmini <nicolagulmini@gmail.com>
2022-05-02 20:04:40 +0100

    update

--- dynamic calibration attempts/density based/dont understand why it does not work.py
+++ dynamic calibration attempts/density based/dont understand why it does not work.py
@@ -14,7 +14,7 @@ AMOUNT_OF_NEW_POINTS = 50
 
 global q, cube, coil_model
 q = Queue(maxsize = AMOUNT_OF_NEW_POINTS)
-cube = CubeModel.cube_to_calib(np.array([-25., 25., 5.]), side_length=50., point_density=10., minimum_number_of_points=50)
+cube = CubeModel.cube_to_calib(np.array([-25., 25., 5.]), side_length=50., point_density=10., minimum_number_of_points=5)
 coil_model = Coil.CoilModel(module_config={'centers_x': [-93.543*1000, 0., 93.543*1000, -68.55*1000, 68.55*1000, -93.543*1000, 0., 93.543*1000], 
                                       'centers_y': [93.543*1000, 68.55*1000, 93.543*1000, 0., 0., -93.543*1000, -68.55*1000, -93.543*1000]}) # mm
 
@@ -23,39 +23,30 @@ def get_theoretical_field(model, point, ori=None):
     if ori is None: return tmp # (3, 8)
     return np.dot(ori, tmp)  
 
-# should be a process
-def update():
-    while len(q.queue) < AMOUNT_OF_NEW_POINTS:
-        pos, ori = cube.origin_corner + cube.side_length*np.random.random(3), np.array([1., 0., 0.]) #np.random.random(3)
-        tmp = get_theoretical_field(coil_model, pos, ori)
-        q.put(np.concatenate((pos, ori, tmp.A1), axis=0))
-    new_raw_points = np.array([q.get() for _ in range(AMOUNT_OF_NEW_POINTS)])
-    cube.add_batch(new_raw_points)
-
 app = dash.Dash()
 app.layout = html.Div(html.Div([html.H4('Magnetic Field Freehand Calibration'),
                                 # html.Div(id='live-update-text'),
                                 dcc.Graph(id='plot'),
                                 dcc.Interval(id='interval-component',
-                                             interval = 2000, # ms
+                                             interval = 1000, # ms
                                              n_intervals = 0)
                                 ]))
 
 fig = make_subplots(horizontal_spacing=0.01, rows=1, cols=3, specs=[[{'is_3d': True}, {'is_3d': True}, {'is_3d':True}]])
 
-color_scale = [[.0, '#FF2D00'], [.5, '#FFF700'], [1.0, '#27FF00']]
+color_scale = [[.0, '#27FF00'], [.5, '#FFF700'], [1.0, '#FF2D00']]
 
 c = np.zeros(cube.xline.shape[0])
 
 # qui metti tutto a zero e' solo per inizializzare
 fig.add_trace(go.Scatter3d(x=cube.xline, y=cube.yline, z=cube.zline, mode='markers',
-                           marker=dict(size=c, color=c, colorscale=color_scale, opacity=.5), 
+                           marker=dict(size=c, color=c, colorscale=color_scale, opacity=.3), 
                            name='x component'), 1, 1)
 fig.add_trace(go.Scatter3d(x=cube.xline, y=cube.yline, z=cube.zline, mode='markers',
-                           marker=dict(size=c, color=c, colorscale=color_scale, opacity=.5), 
+                           marker=dict(size=c, color=c, colorscale=color_scale, opacity=.3), 
                            name='y component'), 1, 2)
 fig.add_trace(go.Scatter3d(x=cube.xline, y=cube.yline, z=cube.zline, mode='markers',
-                           marker=dict(size=c, color=c, colorscale=color_scale, opacity=.5), 
+                           marker=dict(size=c, color=c, colorscale=color_scale, opacity=.3), 
                            name='z component'), 1, 3)
 
 # plot sensor
@@ -74,30 +65,28 @@ fig.update_layout(margin=dict(l=0, r=0, b=0, t=0), showlegend=True, uirevision='
               Input('interval-component', 'n_intervals'))
 def update_graph_live(n_intervals):
     
-    update()
+    while len(q.queue) < AMOUNT_OF_NEW_POINTS:
+        pos, ori = cube.origin_corner + cube.side_length*np.random.random(3), np.random.random(3)
+        tmp = get_theoretical_field(coil_model, pos, ori)
+        q.put(np.concatenate((pos, ori, tmp.A1), axis=0))
+    new_raw_points = np.array([q.get() for _ in range(AMOUNT_OF_NEW_POINTS)])
+    cube.add_batch(new_raw_points)
 
-    # cube is global
-    c_x = cube.contributions.T[0][np.newaxis] 
-    c_y = cube.contributions.T[1][np.newaxis]
-    c_z = cube.contributions.T[2][np.newaxis]
+    # # cube is global
+    c_x, c_y, c_z = cube.contributions.T
 
     unc_x = 1.-np.minimum(c_x, np.ones(c_x.shape))
     unc_y = 1.-np.minimum(c_y, np.ones(c_x.shape))
     unc_z = 1.-np.minimum(c_z, np.ones(c_x.shape))
 
     # update markers' sizes and colors
-    # fig['data'][0]['marker']['color'] = unc_x
-    # fig['data'][0]['marker']['size'] = 60*unc_x
-    # fig['data'][1]['marker']['color'] = unc_y
-    # fig['data'][1]['marker']['size'] = 60*unc_y
-    # fig['data'][2]['marker']['color'] = unc_z
-    # fig['data'][2]['marker']['size'] = 60*unc_z
+    fig['data'][0]['marker']['color'] = unc_x
+    fig['data'][0]['marker']['size'] = 10
+    fig['data'][1]['marker']['color'] = unc_y
+    fig['data'][1]['marker']['size'] = 10
+    fig['data'][2]['marker']['color'] = unc_z
+    fig['data'][2]['marker']['size'] = 10
     
-    fig.add_trace(go.Scatter3d(x=cube.xline, y=cube.yline, z=cube.zline, mode='markers',
-                               marker=dict(size=60*unc_x, color=unc_x, colorscale=[[.0, '#FF2D00'], [.5, '#FFF700'], [1.0, '#27FF00']], opacity=.5), 
-                               name='x component'), 1, 1)
-
-
     return fig
 
 webbrowser.open('http://127.0.0.1:8050/', new=2)
