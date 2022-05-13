4fb775b893576dc716b64a70559bf74cf156784f
Nicola Gulmini <nicolagulmini@gmail.com>
2022-05-13 16:37:01 +0100

    Create prediction comprarison.py

---
+++ dynamic calibration/density based/prediction comprarison.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+# dataset processing
+# take the data
+# shuffle them
+# produce training and validation set
+# test on a diagonal (need to know the dimension of the cube)
+# otherwise produce a test from the same dataset of the training and the validation
+
+# gaussian process
+# find the best parameters
+
+# radial basis function interpolation
+# find the best parameters
+
+# neural netork
+
+# once we have all of them, compare them 
+# nMAE and nRMSE on validation and test
+# time (?)
+# uncertainty
+# correlation between uncertainty and error (only for RBFI and GP)
